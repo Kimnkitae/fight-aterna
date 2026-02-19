f024e00033916ad930b367f450d906187e335a81
Kimnkitae <kimnkitae@gmail.com>
2026-02-19 21:01:37 +0300

    Фон, движение и взаимодействие для hospital

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import kaplay from "kaplay";
-import { Prolog } from "/src/prolog/scene1.js";
+import kaplay from "kaplay"
+import { Prolog } from "/src/prolog/scene1.js"
 
 
 kaplay({
     width: 1920,
     height: 1080,
     canvas: document.querySelector("#canvas"),
-});
+})
 
-Prolog();
-go("scene1");
+Prolog()
+go("scene1")
--- src/prolog/scene1.js
+++ src/prolog/scene1.js
@@ -1,70 +1,127 @@
 export const Prolog = () => {
 
-    loadSprite("bgd-hospital", "sprites/prolog/background/hospital.png");
-    loadSprite("floor-hospital", "sprites/prolog/background/hospital-floor.png");
-    loadSprite("left-wall-hospital", "sprites/prolog/background/hospital-left-wall.png");
-    loadSprite("right-wall-hospital", "sprites/prolog/background/hospital-right-wall.png");
+    loadSprite("bgd-hospital", "sprites/prolog/background/hospital.png")
+    loadSprite("floor-hospital", "sprites/prolog/background/hospital-floor.png")
+    loadSprite("left-wall-hospital", "sprites/prolog/background/hospital-left-wall.png")
+    loadSprite("right-wall-hospital", "sprites/prolog/background/hospital-right-wall.png")
+    loadSprite("hospital-grandfather", "sprites/prolog/background/hospital-grandfather.png")
+    loadSprite("button-e", "sprites/prolog/BUTTON/e/button-e.png", {
+        sliceX: 10,
+        sliceY: 1,
+        anims: {
+            animation: {from: 0, to: 9, loop: true}
+        }
+    })
     
     /* PLAYER */
-    loadSprite("main-character", "sprites/main-character/idle/idle.png");
-    loadSprite("player-walk", "sprites/main-character/walk/Walk.png", {
-        sliceX: 1,
-        sliceY: 7,
+    
+    loadSprite("player", "sprites/main-character/player.png", {
+        sliceX: 9,
+        sliceY: 1,
         anims: {
 
-            walkD: {from: 0, to: 6, loop: true },
+            idle: {from: 0, to: 0},
+
+            walkD: {from: 1, to: 8, loop: true, speed: 10},
         },
-    });
+    })
 
     scene("scene1", () => {
 
         const hospital = add([
             sprite("bgd-hospital"),
-        ]);
+        ])
 
-        const hospital_floor = add([
+        const hospitalFloor = add([
             sprite("floor-hospital"),
             pos(0, 950),
             area(),
             fixed(),
             body({ isStatic: true }),
-        ]);
+        ])
 
-        const hospital_left_wall = add([
+        const hospitalLeftWall = add([
             sprite("left-wall-hospital"),
             pos(0, 225),
             area(),
             fixed(),
             body({ isStatic: true }),
-        ]);
+        ])
 
-        const hospital_right_wall = add([
+        const hospitalright_wall = add([
             sprite("right-wall-hospital"),
             pos(1800, 260),
             area(),
             fixed(),
             body({ isStatic: true }),
-        ]);
+        ])
+
+        const hospitalDed = add([
+            sprite("hospital-grandfather"),
+            pos(1480, 700),
+            area(),
+            body(),
+            body({ isStatic: true}),
+            "grandpa",
+        ])
 
         /* PLAYER */
+        let currentAnim = "idle"
         
-        const player = add([
-            sprite("main-character"),
+        const mainCharacter = add([
+            sprite("player"),
             pos(600,618),
-            "player",
-        ]);
+            area(),
+            body(),
+            "player"
+        ])
 
         const WalkPlayer = (key) => {
             if(key == "d" || key == "в") {
-                player.move(2000, 0);
-                player.play("walkD");
-
+                mainCharacter.flipX = false
+                mainCharacter.move(200, 0)
+                if(currentAnim !== "walkD") {
+                    mainCharacter.play("walkD")
+                    currentAnim = "walkD"
+                }
             }
+            if(key == "a" || key == "ф") {
+                mainCharacter.flipX = true
+                mainCharacter.move(-300, 0)
+                if(currentAnim !== "walkD") {
+                    mainCharacter.play("walkD")
+                    currentAnim = "walkD"
+                }
+            }
+            
         }
 
-
-        onKeyPress((key) => {
-            WalkPlayer(key);
+        onKeyDown((key) => {
+            WalkPlayer(key)
         })
-    });
-};
+
+        onKeyRelease(["d", "в"], () => {
+            mainCharacter.play("idle")
+            currentAnim = "idle"
+        })
+
+        onKeyRelease(["a", "ф"], () => {
+            mainCharacter.play("idle")
+            currentAnim = "idle"
+        })
+
+
+
+
+        onCollide("player", "grandpa", () => {
+            const e = add([
+                sprite("button-e"),
+                pos(1600, 600),
+            ])
+            e.play("animation")
+            onCollideEnd("player", "grandpa", () => {
+                destroy(e)
+            })
+        })
+    })
+}
