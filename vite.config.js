6462e0514998a1464c0caedd5659737e10c23146
Kimnkitae <kimnkitae@gmail.com>
2026-03-18 20:25:00 +0300

    Финальная версия игры

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ const kaplayCongrats = () => {
 
 export default defineConfig({
     // index.html out file will start with a relative path for script
-    base: "/fight-aterna",
+    base: "./",
     server: {
         port: 3001,
     },
